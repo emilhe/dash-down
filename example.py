65cd58090354f93790a9b34c046ba0e498768eb0
emher <emil.h.eriksen@gmail.com>
2022-03-23 22:22:19 +0100

    Working on tests

--- example.py
+++ example.py
@@ -6,3 +6,5 @@ blueprint = md_to_blueprint_dmc('test/markdown_test.md')
 if __name__ == '__main__':
     app = DashProxy(blueprint=blueprint)
     app.run_server(port=7899, debug=True)
+
+# test stuff
