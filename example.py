aceb6e7d465c7e95a24fe946c8228ab5c627a1c4
Emil Haldrup Eriksen <emil.h.eriksen@gmail.com>
2022-03-23 22:43:27 +0100

    Update example.py

--- example.py
+++ example.py
@@ -5,6 +5,4 @@ blueprint = md_to_blueprint_dmc('tests/markdown_test.md')
 
 if __name__ == '__main__':
     app = DashProxy(blueprint=blueprint)
-    app.run_server(port=7899, debug=True)
-
-# test stuff
+    app.run_server()
